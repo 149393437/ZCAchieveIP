4b62a6021efd6abbf9ab2c44e7febf026b117f00
149393437 <zhangcheng@qq.com>
2014-10-18 14:28:05 +0800

    提交
    提交

---
+++ SDK包/ZCAchieveIP.h
@@ -0,0 +1,13 @@
+//
+//  ZCAchieveIP.h
+//  Socket_TCP_demo
+//
+//  Created by ZhangCheng on 14-4-11.
+//  Copyright (c) 2014年 zhangcheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZCAchieveIP : NSObject
++(NSString *)getWiFiIPAddress;
+@end
---
+++ SDK包/ZCAchieveIP.m
@@ -0,0 +1,45 @@
+
+//
+//  ZCAchieveIP.m
+//  Socket_TCP_demo
+//
+//  Created by ZhangCheng on 14-4-11.
+//  Copyright (c) 2014年 zhangcheng. All rights reserved.
+//
+
+#import "ZCAchieveIP.h"
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <net/if.h>
+
+#include <ifaddrs.h>
+#import <dlfcn.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+@implementation ZCAchieveIP
+
++(NSString *)getWiFiIPAddress
+{
+    BOOL success;
+    struct ifaddrs * addrs;
+    const struct ifaddrs * cursor;
+    
+    success = getifaddrs(&addrs) == 0;
+    if (success) {
+        cursor = addrs;
+        while (cursor != NULL) {
+            // the second test keeps from picking up the loopback address
+            if (cursor->ifa_addr->sa_family == AF_INET && (cursor->ifa_flags & IFF_LOOPBACK) == 0)
+            {
+                NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
+                if ([name isEqualToString:@"en0"])  // Wi-Fi adapter
+                    return [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)cursor->ifa_addr)->sin_addr)];
+            }
+            cursor = cursor->ifa_next;
+        }
+        freeifaddrs(addrs);
+    }
+    return nil;
+}
+
+@end
